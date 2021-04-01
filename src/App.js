c6b0745d480d66e27def6d360bdb889f9f9f5552
David Fugimoto <dsfugimoto@gmail.com>
2021-04-01 16:17:15 -0300

    Feat: creating the test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Question1 from './Question1/Question1';
+import Question2 from './Question2/Question2';
+import Question3 from './Question3/Question3';
+import Question4 from './Question4/Question4.answer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <Question1 />
+        <Question2 />
+        <Question3 />
+        <Question4 />
       </header>
     </div>
   );
---
+++ src/Question4/Question4.js
@@ -0,0 +1,26 @@
+
+
+const Question4 = () => {
+
+	return (
+		<>
+			<h1>Question 4</h1>
+			<p>Create a component that makes an <u>external HTTP Request </u>to fetch user data as soon as it loads. Then iterate through 
+				the fetched data and display the picture thumbanail and full name of the person.
+				Lastly, create a simple pagination with a previous and next buttons to fetch different pages of data. For this exercise, take these into account:
+			</p>
+			<ul>
+				<li>The url to get the data is: https://randomuser.me/api/?page=X&results=Y&seed=foobar</li>
+				<li>The <code>results</code> parameter indicates the number of items you want to return;</li>
+				<li>The <code>page</code> parameter indicates the page you want to return;</li>
+				<li>The <code>seed</code> parameter is to always return the same results (don't need to touch this parameter);</li>
+				<li>The first page should be 1;</li>
+				<li>There's an unlimited number of pages (don't worry about the total number of items);</li>
+				<li>Each page should return 5 items.</li>
+			</ul>
+			{/* Your code here */}
+		</>
+	)
+}
+
+export default Question4
