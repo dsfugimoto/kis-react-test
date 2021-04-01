15f160b2a115e09e5e3006d101141ab2d345f841
David Fugimoto <dsfugimoto@gmail.com>
2021-04-01 16:27:56 -0300

    Fix: formatting text

--- src/Question4/Question4.js
+++ src/Question4/Question4.js
@@ -11,9 +11,9 @@ const Question4 = () => {
 			</p>
 			<ul>
 				<li>The url to get the data is: https://randomuser.me/api/?page=X&results=Y&seed=foobar</li>
-				<li>The <code>results</code> parameter indicates the number of items you want to return;</li>
-				<li>The <code>page</code> parameter indicates the page you want to return;</li>
-				<li>The <code>seed</code> parameter is to always return the same results (don't need to touch this parameter);</li>
+				<li>The <i>results</i> parameter indicates the number of items you want to return;</li>
+				<li>The <i>page</i> parameter indicates the page you want to return;</li>
+				<li>The <i>seed</i> parameter is to always return the same results (don't need to touch this parameter);</li>
 				<li>The first page should be 1;</li>
 				<li>There's an unlimited number of pages (don't worry about the total number of items);</li>
 				<li>Each page should return 5 items.</li>
