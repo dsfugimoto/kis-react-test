61730948df1a4763f081ad277ae644d1d52fa48d
phelip-roberto <phelip.souza@ger7.com.br>
2021-04-03 18:15:20 -0300

    Resolve question1

--- src/Question1/Question1.js
+++ src/Question1/Question1.js
@@ -1,25 +1,28 @@
-import { useState } from "react"
+import { createRef } from "react";
 
 const Question1 = () => {
-	const [content, setContent] = useState('')
+  const content = createRef();
 
-	const handleChange = e => {
-		setContent(e.target.value);
-	}
+  const handleSubmit = (event) => {
+    showAlert();
+    event.preventDefault();
+  }
 
-	const showAlert = () => {
-		alert(content);
-	}
+  const showAlert = () => {
+    alert(content.current.value);
+  }
 
-	return (
-		<>
-			<h1>Question 1</h1>
-			<p>Transform this <b>Controlled Component</b> into an <u>Uncontrolled Component</u> that renders a text input and a button. 
+  return (
+    <>
+      <h1>Question 1</h1>
+      <p>Transform this <b>Controlled Component</b> into an <u>Uncontrolled Component</u> that renders a text input and a button.
 			When clicking the button, this component should alerts whatever data we have inside the input. We should focus the input as soon as the components renders:</p>
-			<input type="text" value={content} onChange={handleChange} />
-			<button onClick={showAlert}>Alert!</button>
-		</>
-	)
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={content} />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  )
 }
 
 export default Question1
