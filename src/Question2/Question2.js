1abf538afabbacead8cfe8b6d3249a43fc9e7805
phelip-roberto <phelip.souza@ger7.com.br>
2021-04-03 18:16:23 -0300

    Resolve question2

--- src/Question2/Question2.js
+++ src/Question2/Question2.js
@@ -1,16 +1,52 @@
+import React from "react"
 
 
 const Question2 = () => {
 
-	return (
-		<>
-			<h1>Question 2</h1>
-			<p>Create a component that renders an input, a button and a list of items. You can add items typing in the input and clicking the button.
-				You can also remove an item from the list upon clicking it.
+  const [list, setList] = React.useState([]);
+  const [name, setName] = React.useState('');
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleAdd() {
+    const newList = list.concat({ name });
+
+    setList(newList);
+  }
+
+  function handleRemove(item) {
+    const name = item
+    const newList = list.filter(item => item.name !== name);
+
+    setList(newList);
+  }
+
+  return (
+    <>
+      <h1>Question 2</h1>
+      <p>Create a component that renders an input, a button and a list of items. You can add items typing in the input and clicking the button.
+      You can also remove an item from the list upon clicking it.
 			</p>
-			{/* Your code here */}
-		</>
-	)
+      {
+        <div>
+          <div>
+            <input type="text" value={name} onChange={handleChange} />
+            <button type="button" onClick={handleAdd}>
+              Add
+        </button>
+          </div>
+
+          <ul>
+            {list.map((item) => (
+              <li key={item.name} onClick={() => handleRemove(item.name)}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+      }
+    </>
+  )
 }
 
 export default Question2
